4eb81057a018c4d0869d93a65cd855e85b3936c5
Ritik sharma <ritik1832004@gmail.com>
2025-01-01 14:39:37 +0530

    hell

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./vite-react_bg_changer-deploy"
+  // base: "./vite-react_bg_changer-deploy"
 })
